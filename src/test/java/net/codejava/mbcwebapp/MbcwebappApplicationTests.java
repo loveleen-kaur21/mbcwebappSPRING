9f3ff7ce11301aba6524cb13f13c73887961c7fb
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2021-06-07 15:21:17 -0500

    initiation

---
+++ src/test/java/net/codejava/mbcwebapp/MbcwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.mbcwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbcwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
